9043f0fb6acd589737a1cea160548b3d7e5d4e3b
David Arel <darel@frima.local>
2016-05-17 15:45:26 -0400

    menu system

---
+++ Block Breaker/Assets/Scripts/HamburgerMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HamburgerMenu : MonoBehaviour
+{
+    public Sprite pressedSprite;
+    private Image myImage;
+
+    public void Awake()
+    {
+        this.myImage = GetComponent<Image>();
+    }
+
+    public void OnClick()
+    {
+        myImage.sprite = pressedSprite;
+    }
+}
---
+++ Block Breaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void loadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void quitGame()
+    {
+        //Seulement pour PC
+        Application.Quit();
+    }
+}
