cf5144d91a4933402a1894be251489ccd1dd44af
David Arel <darel@frima.local>
2016-05-17 16:23:00 -0400

    background music

---
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour
+{
+    static  GameObject instance;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    if (instance != null)
+	    {
+	        Destroy(gameObject);
+	    }
+	    else
+	    {
+            instance = gameObject;
+            GameObject.DontDestroyOnLoad(instance);
+        }
+    }
+}
